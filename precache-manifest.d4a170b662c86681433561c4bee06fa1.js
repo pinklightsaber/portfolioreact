128d7b696d6a7cdce23e1d02f02c279624cf91e2
unknown <ctorres@idemar.cl>
2020-09-07 18:00:35 -0300

    Updates

---
+++ precache-manifest.d4a170b662c86681433561c4bee06fa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c266c9700c1f2d9f93ea95657322934",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "abfe121df0dd10040c6e",
+    "url": "/portfolioreact/static/css/main.9a5640bb.chunk.css"
+  },
+  {
+    "revision": "0fb913341da08b3ec573",
+    "url": "/portfolioreact/static/js/2.33427e44.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/portfolioreact/static/js/2.33427e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfe121df0dd10040c6e",
+    "url": "/portfolioreact/static/js/main.9cf1204c.chunk.js"
+  },
+  {
+    "revision": "ccc82b9744967cf75754",
+    "url": "/portfolioreact/static/js/runtime-main.45fc4603.js"
+  },
+  {
+    "revision": "76979f9abe7411db6a5cbc930e94e07b",
+    "url": "/portfolioreact/static/media/a1c27395998927.5eaf172cdca6d.76979f9a.jpg"
+  },
+  {
+    "revision": "b10a1a8c6513316057633a1636d29599",
+    "url": "/portfolioreact/static/media/mine2.b10a1a8c.jpg"
+  }
+]);
